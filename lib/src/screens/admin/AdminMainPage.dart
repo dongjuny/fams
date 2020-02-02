c786d81d109e57db7d3cbfac0170543a45644399
Ujoy7851 <joy7851@gmail.com>
2020-02-02 22:33:55 +0900

    refactor class name of admin main page

--- lib/src/screens/admin/AdminMainPage.dart
+++ lib/src/screens/admin/AdminMainPage.dart
@@ -22,7 +22,6 @@ class Cards {
 
 class _AdminMainState extends State<AdminMain> with TickerProviderStateMixin {
 
-  var test = "test";
   var appColors = [Color.fromRGBO(231, 129, 109, 1.0),Color.fromRGBO(99, 138, 223, 1.0),Color.fromRGBO(111, 194, 173, 1.0)];
   var currentColor = Color.fromRGBO(231, 129, 109, 1.0);
   var cardIndex = 0;
