3071540f290ce9657e2c0ff8db21f249734aedd7
Ujoy7851 <joy7851@gmail.com>
2020-02-05 21:45:34 +0900

    update admin detail page

--- lib/src/screens/admin/AdminDetailPage.dart
+++ lib/src/screens/admin/AdminDetailPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 
 class AdminDetailPage extends StatefulWidget {
   @override
@@ -17,8 +18,11 @@ class _AdminDetailState extends State<AdminDetailPage>{
 
   var currentColor = Color.fromRGBO(231, 129, 109, 1.0);
   var cardIndex = 0;
-  ScrollController scrollController;
-  List<User> cardsList = [
+
+  String start_time = "Not set";
+  String end_time = "Not set";
+
+  List<User> usersList = [
     User('user A', true),
     User('user B', true),
     User('user C', false),
@@ -31,6 +35,7 @@ class _AdminDetailState extends State<AdminDetailPage>{
   ];
 
   AnimationController animationController;
+  ScrollController scrollController;
   ColorTween colorTween;
   CurvedAnimation curvedAnimation;
 
@@ -55,39 +60,251 @@ class _AdminDetailState extends State<AdminDetailPage>{
       ),
       body: new Center(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Row(),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 0.0),
-              child: Text("Group A", style: TextStyle(fontSize: 20.0, color: Colors.white, fontWeight: FontWeight.w400)),
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 0.0),
+                  child: Text("Setting for Group A", style: TextStyle(fontSize: 15.0, color: Colors.white, fontWeight: FontWeight.w400)),
+                ),
+              ],
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 0.0),
-              child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 5.0),
+              child: Column(
+                children: <Widget>[
+                  Card(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Icon(Icons.add, color: Colors.transparent,),
+                              Text("Users", style: TextStyle(fontSize: 15.0, color: Colors.grey),),
+                              Icon(Icons.add, color: Colors.grey,),
+                            ],
+                          ),
+                        ),
+                        Divider(),
+                        Container(
+                          height: 300.0,
+                          child: ListView.builder(
+                            itemBuilder: (context, position) {
+                              User user = usersList[position];
+                              return Column(
+                                children: <Widget>[
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      children: <Widget>[
+                                        Icon(Icons.account_circle, size: 35.0, color: Colors.grey,),
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                                          child: Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: <Widget>[
+                                              Row(
+                                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                                children: <Widget>[
+                                                  Text(user.name, style: TextStyle(fontWeight: FontWeight.w500, fontSize: 15.0),),
+                                                ],
+                                              ),
+                                              Padding(
+                                                padding: const EdgeInsets.only(top: 1.0),
+                                                child: Text(
+                                                  user.isAttend ? 'Attend' : 'Absent',
+                                                  style: TextStyle(color: user.isAttend ? Colors.green : Colors.red, fontSize: 13.0),
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Divider(),
+                                ],
+                              );
+                            },
+                            itemCount: usersList.length,
+                          ),
+                        ),
+                      ],
+                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)
+                    ),
+                  ),
+                ],
+              )
+            ),
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Card(
-                      child: Container(
-                        width: 400.0,
-                        height: 200.0,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-
-                          ],
-                        ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text("Time Setting", style: TextStyle(fontSize: 15.0, color: Colors.grey),),
+                              ],
+                            ),
+                          ),
+                          Divider(),
+                          Container(
+                            height: 130.0,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Row(
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                                      child: Text("Start", style: TextStyle(fontSize: 15.0),),
+                                    ),
+                                    RaisedButton(
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(5.0)),
+                                      elevation: 4.0,
+                                      onPressed: () {
+                                        DatePicker.showTimePicker(context,
+                                            theme: DatePickerTheme(
+                                              containerHeight: 210.0,
+                                            ),
+                                            showTitleActions: true, onConfirm: (time) {
+                                              print('confirm $time');
+                                              start_time = '${time.hour} : ${time.minute} : ${time.second}';
+                                              setState(() {});
+                                            }, currentTime: DateTime.now(), locale: LocaleType.en);
+                                        setState(() {});
+                                      },
+                                      child: Container(
+                                        alignment: Alignment.center,
+                                        height: 50.0,
+                                        child: Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                          children: <Widget>[
+                                            Row(
+                                              children: <Widget>[
+                                                Container(
+                                                  child: Row(
+                                                    children: <Widget>[
+                                                      Icon(
+                                                        Icons.access_time,
+                                                        size: 18.0,
+                                                        color: Colors.teal,
+                                                      ),
+                                                      Text(
+                                                        " $start_time",
+                                                        style: TextStyle(
+                                                            color: Colors.teal,
+                                                            fontWeight: FontWeight.bold,
+                                                            fontSize: 18.0),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                )
+                                              ],
+                                            ),
+                                            Text(
+                                              "  Change",
+                                              style: TextStyle(
+                                                  color: Colors.teal,
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 18.0),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      color: Colors.white,
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                                      child: Text("end  ", style: TextStyle(fontSize: 15.0),),
+                                    ),
+                                    RaisedButton(
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(5.0)),
+                                      elevation: 4.0,
+                                      onPressed: () {
+                                        DatePicker.showTimePicker(context,
+                                            theme: DatePickerTheme(
+                                              containerHeight: 210.0,
+                                            ),
+                                            showTitleActions: true, onConfirm: (time) {
+                                              print('confirm $time');
+                                              end_time = '${time.hour} : ${time.minute} : ${time.second}';
+                                              setState(() {});
+                                            }, currentTime: DateTime.now(), locale: LocaleType.en);
+                                        setState(() {});
+                                      },
+                                      child: Container(
+                                        alignment: Alignment.center,
+                                        height: 50.0,
+                                        child: Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                          children: <Widget>[
+                                            Row(
+                                              children: <Widget>[
+                                                Container(
+                                                  child: Row(
+                                                    children: <Widget>[
+                                                      Icon(
+                                                        Icons.access_time,
+                                                        size: 18.0,
+                                                        color: Colors.teal,
+                                                      ),
+                                                      Text(
+                                                        " $end_time",
+                                                        style: TextStyle(
+                                                            color: Colors.teal,
+                                                            fontWeight: FontWeight.bold,
+                                                            fontSize: 18.0),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                )
+                                              ],
+                                            ),
+                                            Text("  Change",
+                                              style: TextStyle(
+                                                  color: Colors.teal,
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 18.0),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      color: Colors.white,
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            )
+                          )
+                        ],
                       ),
                       shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10.0)
                       ),
                     ),
                   ],
-                ),
-              ),
+                )
             ),
           ],
         ),
